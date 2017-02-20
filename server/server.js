2c834c1ac3f323d1689cd746960e93950ea3522d
Jose Marcos Leyva Castro <jmarcoslc@gmail.com>
2017-02-20 20:06:02 +0100

    Añadido git ignore

--- server/server.js
+++ server/server.js
@@ -2,14 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var path = require("path");
 var io = require('socket.io')(http);
+var dl = require('delivery');
+var fs  = require('fs');
 
 var port = process.env.PORT || '3000';
 
-app.get('/', function(req, res){
+app.use(express.static('static'));
+app.get('/', function(req, res) {
   res.sendFile(path.normalize(__dirname + "/../static/index.html"));
 });
 
 io.on('connection', function(socket){
+  var delivery = dl.listen(socket);
+  delivery.on('receive.success',function(file){
+    var params = file.params;
+    fs.writeFile(path.normalize(__dirname + "/../static/"+file.name),file.buffer, function(err){
+      if(err){
+        console.log('File could not be saved.');
+      }else{
+        console.log('File saved.');
+      };
+    });
+  });
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
@@ -19,6 +33,20 @@ io.on('connection', function(socket){
     io.emit('chat_message', msg);
   });
 });
+ 
+/*io.sockets.on('connection', function(socket){
+  
+  delivery.on('receive.success',function(file){
+    var params = file.params;
+    fs.writeFile(file.name,file.buffer, function(err){
+      if(err){
+        console.log('File could not be saved.');
+      }else{
+        console.log('File saved.');
+      };
+    });
+  });
+});*/
 
 http.listen(port, function(){
   console.log('listening on *:'+port);
