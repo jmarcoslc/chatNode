08002e0599a0dc2d72f78870444f9035054dd869
Jose Marcos Leyva Castro <jmarcoslc@gmail.com>
2017-02-21 22:14:30 +0100

    Se muestran usuarios en línea

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
-  var delivery = dl.listen(socket);
+/*  var delivery = dl.listen(socket);
   delivery.on('receive.success',function(file){
     var params = file.params;
     fs.writeFile(path.normalize(__dirname + "/../static/"+file.name),file.buffer, function(err){
@@ -24,14 +24,18 @@ io.on('connection', function(socket){
         console.log('File saved.');
       };
     });
-  });
-  console.log('a user connected');
+  });*/
+  console.log('User conected');
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('User disconnected');
   });
   socket.on('chat_message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat_message', msg);
+  });
+  socket.on('keepAlive', function(msg){
+    console.log('keepAlive: ' + msg.user);
+    io.emit('keepAlive', msg);
   });
 });
  
--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,58 @@
 var user_name = "Anonymouse";
+var state = "Not specified";
+var logged = false;
+var users_online = new Array();
 
-function addListeners() {
+function addListeners(socket) {
 	$(".modal-footer .btn").click(function() {
 		user_name = $('.form_username_input').val();
-		alert(user_name);
+		state = $("#form_state option:selected").text();
 		$(".user-name").text(user_name);
 		$("#modal1").modal("close");
 		$(".login").css({
 			display: 'none'
 		});
+		logged = true;
+		keepAlive(socket);
 	});
 }
 
+function keepAlive(socket) {
+	var func_ka = function(){
+		if (logged) {
+			socket.emit('keepAlive', {user: user_name, stat: state});
+		}
+	}
+
+	func_ka();
+	setInterval(func_ka, 10000);
+}
+
 $(document).ready(function() {
+	var socket = io();
 	$('.modal').modal();
 	$('#modal1').modal('open');
-	addListeners();
-	var socket = io();
+	$('select').material_select();
+	addListeners(socket);
 
 	$('form').submit(function() {
 	    socket.emit('chat_message', {msg:$('#input-message-content').val(), user:user_name});
 	    $('#input-message-content').val('');
 	    return false;
-	  });
+	});
 
-	  socket.on('chat_message', function(msg){
+	socket.on('chat_message', function(msg){
+		var msg_class = "message";
+		var wrap_right_class = "";
+		
+		if (msg.user == user_name) {
+			msg_class = "self-message";
+			wrap_right_class = " alg-right"
+		}
+
 	    $('.chat-content').append(
-	    	'<div class="message-wrapp">\
-				<span class="message">\
+	    	'<div class="message-wrapp'+wrap_right_class+'">\
+				<span class="'+msg_class+'">\
 					<span class="message-header">\
 						<span class="message-name">'+msg.user+'</span>\
 					</span>\
@@ -36,6 +61,18 @@ $(document).ready(function() {
 					</div>\
 				</span>\
 			</div>');
+	});
+
+	socket.on("keepAlive", function(msg) {
+		if (msg.user != user_name && $.inArray(msg.user, users_online) == -1) {
+			users_online.push(msg.user);
+			$('.chats-wrapp').append('<div class="chat-pan-content">\
+				<img class="circle chat-avatar" src="img/no-avatar.png">\
+				<span class="chat-name">'+msg.user+'</span>\
+				</div>'
+			);
+			Materialize.toast(msg.user + ' se ha conectado', 3000, 'rounded')
+		}
 	});
 /*	  socket.on('connect', function(){
 	    var delivery = new Delivery(socket);
